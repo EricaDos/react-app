8f69602cb5a525a70cd1f8e23da7a8cd83237147
Designs By Dos <ericadossantos2277@gmail.com>
2021-05-06 15:54:10 +0100

    Completed Jobs Backend

--- main-app/backend/routes/jobs.js
+++ main-app/backend/routes/jobs.js
@@ -1,38 +1,28 @@
 const router = require('express').Router();
 let Job = require('../models/job.models');
 
-//Incoming HTTPS get request
+//CRUD Application of jobs
+
 
 router.route('/').get((req, res) => {
     Job.find()
+        .sort({ date: -1 }) //Descending date displays
         .then(jobs => res.json(jobs))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+router.route('/').post((req, res) => {
+  const newJob = new Job({
+    name: req.body.name
+  });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const description = req.body.username;
-
-
-    const newJob = new Job({
-        username,
-        description
-    })
-
-    newJob.save()
-        .then(() => res.json('Job added!'))
-        .catch(err => res.status(400).json('Error:' + err));
+  newJob.save().then(job => res.json(job))
 });
 
-//CRUD Application of jobs
-router.route('/:id').get((req, res) => {
-    Job.findById()
-        .then(jobs => res.json(jobs))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
+
+
 router.route('/:id').delete((req, res) => {
-    Job.findByIdAndDelete(req.params.id)
-        .then(jobs => res.json('Job Deleted'))
+    Job.findByIdAndDelete(req.params.id) //Fetching from uri
+        .then(jobs => res.json('Job Deleted')) //Ensures its able to delette
         .catch(err => res.status(400).json('Error: ' + err));
 });
 router.route('/update/:id').post((req, res) => {
